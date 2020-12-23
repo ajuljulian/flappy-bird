e6f35dd75b8dec3be96bf64552fea93b78ddbe9f
Ara Juljulian <ajuljulian@yahoo.com>
2020-12-23 09:06:00 -0800

    add animated floor.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame, sys
 
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 900))
+    screen.blit(floor_surface, (floor_x_pos+SCREEN_WIDTH, 900))
+
 pygame.init()
 
 SCREEN_WIDTH = 576
@@ -14,6 +18,10 @@ FPS = 120
 bg_surface = pygame.image.load('sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 
+floor_surface = pygame.image.load('sprites/base.png').convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+floor_x_pos = 0
+
 while True:
 
     for event in pygame.event.get():
@@ -22,5 +30,11 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface, (0, 0))
+
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -SCREEN_WIDTH:
+        floor_x_pos = 0
+    
     pygame.display.update()
     clock.tick(FPS)
