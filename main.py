7890b962e65b7eeb3829a58cffcd4e25e9001e36
Ara Juljulian <ajuljulian@yahoo.com>
2020-12-23 11:36:47 -0800

    basic pipe animation.

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ def draw_floor():
     screen.blit(floor_surface, (floor_x_pos, 900))
     screen.blit(floor_surface, (floor_x_pos+SCREEN_WIDTH, 900))
 
+def create_pipe():
+    new_pipe = pipe_surface.get_rect(midtop = (700, 512))
+    return new_pipe
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 5
+    return pipes
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        screen.blit(pipe_surface, pipe)
+
 pygame.init()
 
 SCREEN_WIDTH = 576
@@ -19,16 +32,27 @@ bird_movement = 0
 
 FPS = 120
 
+# Background
 bg_surface = pygame.image.load('sprites/background-day.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 
+# Floor
 floor_surface = pygame.image.load('sprites/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
+# Bird
 bird_surface = pygame.image.load('sprites/bluebird-midflap.png').convert()
 bird_surface = pygame.transform.scale2x((bird_surface))
 bird_rect = bird_surface.get_rect(center=(100, SCREEN_HEIGHT/2))
+
+# Pipes
+pipe_surface = pygame.image.load('sprites/pipe-green.png').convert()
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200) # in milliseconds
 
 while True:
 
@@ -39,13 +63,21 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_movement = -12
+        if event.type == SPAWNPIPE:
+            pipe_list.append(create_pipe())
 
     screen.blit(bg_surface, (0, 0))
 
-    bird_movement += 0.25
+    # Bird
+    bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird_surface, bird_rect)
 
+    # Pipes
+    pipe_list = move_pipes(pipe_list)
+    draw_pipes(pipe_list)
+
+    # Floor
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -SCREEN_WIDTH:
